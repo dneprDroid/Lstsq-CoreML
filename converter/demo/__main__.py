3216ea7aca666e3cc5db672f8b23111107c3022e
alex <ovechko.056@gmail.com>
2024-06-04 17:36:29 +0200

    Demo for converter module

---
+++ converter/demo/__main__.py
@@ -0,0 +1,10 @@
+"""
+Run in the root dir:
+
+python3 -m converter.demo
+
+"""
+
+from .convert import main
+
+main()
---
+++ converter/demo/convert.py
@@ -0,0 +1,135 @@
+"""
+Run in the root dir:
+
+python3 -m converter.demo
+
+"""
+import os
+import shutil
+import json
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision.ops.deform_conv import deform_conv2d
+
+import coremltools
+
+from .. import register_op
+
+
+class TestModel(nn.Module):
+
+    def forward(self, a, b):
+        # result = torch.linalg.lstsq(a, b, driver='gelsd')
+        # return result.solution, \
+        #     result.residuals, \
+        #     result.rank, \
+        #     result.singular_values
+        solution, residuals, rank, singular_values = torch.linalg.lstsq(a, b, driver='gelsd')
+        # print(f"rank: ", rank.shape)
+        # raise f"rank: ${rank}"
+
+        # sizeInfo = torch.zeros(3, 1)
+        # sizeInfo[0] = solution.numel()
+        # sizeInfo[1] = rank.numel()
+        # sizeInfo[2] = singular_values.numel()
+
+        return torch.cat([
+            # sizeInfo.flatten(),
+            
+            solution.flatten(), 
+            # residuals.flatten(),
+            rank.flatten(), 
+            singular_values.flatten()
+        ])
+
+
+def rm(path):
+    if not os.path.exists(path):
+        return
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        os.remove(path)
+
+
+def save_as_json(tensors, filename, output_dir):
+    values = [tensor.numpy().tolist() for tensor in tensors]
+    values_str = json.dumps(values)
+
+    path = os.path.join(output_dir, filename)
+    with open(path, 'w') as file:
+        file.write(values_str)
+
+def convert(output_dir, filename='test-model'):
+    torch.return_types.linalg_lstsq
+
+    output_path = os.path.join(output_dir, filename)
+
+    torch_model = TestModel()
+
+    print("generating random input tensor...")
+    
+    a = torch.rand(2, 4, 3, 5).type(torch.float32)
+    b = torch.rand(2, 4, 3).type(torch.float32)
+
+    example_input = (a, b)
+    example_output = torch_model(a, b)
+
+    print("example output: ", example_output.shape, example_output)
+
+    save_as_json(a, 'example_input_a.json', output_dir)
+    save_as_json(b, 'example_input_b.json', output_dir)
+    
+    save_as_json((example_output,), 'example_output.json', output_dir)
+
+    traced_model = torch.jit.trace(torch_model, example_input)
+
+    input_name = "input"
+    output_name = "output"
+
+    mil_inputs = [
+        coremltools.TensorType(
+            name="%s-%i" % (input_name, input_index),
+            shape=(x.shape)
+        )
+        for input_index, x in enumerate(example_input)
+    ]
+    mil_outputs = [
+        coremltools.TensorType(
+            name=output_name
+        )
+    ]
+    print("mil_inputs: ", mil_inputs)
+    print("mil_outputs: ", mil_outputs)
+
+    mlmodel = coremltools.convert(
+        traced_model,
+        inputs=mil_inputs,
+        outputs=mil_outputs,
+        convert_to="neuralnetwork"
+    )
+    mlmodel_path = output_path + ".mlmodel"
+
+    out_pb_path = mlmodel_path + ".pb"
+
+    rm(mlmodel_path)
+    rm(output_path)
+    rm(out_pb_path)
+
+    mlmodel.save(mlmodel_path)
+
+    shutil.copyfile(mlmodel_path, out_pb_path)
+
+    print(f"Saved to {output_dir}")
+
+
+def main():
+    register_op()
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    out_dir_path = os.path.join(current_dir, "../../DemoApp/generated")
+    out_dir_path = os.path.abspath(out_dir_path)
+    convert(out_dir_path)
